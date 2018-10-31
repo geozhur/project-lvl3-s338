a25b77a89321678290b8496417744ffed57d0126
Santa Klaus <egorich2004@mail.ru>
2018-10-31 08:36:14 +0100

    fix error

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -47,7 +47,7 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $e)
     {
 
-        if($e instanceof NotFoundHttpException){
+        if ($e instanceof NotFoundHttpException) {
             return response(view("errors.404"), 404);
         }
 
