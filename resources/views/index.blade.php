4ab8c12d10380cf364bbc25e5c472e06bed1e966
Santa Klaus <egorich2004@mail.ru>
2018-11-02 09:00:24 +0100

    add text to index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,4 +2,14 @@
 
 @section('content')
 @include('layouts.form')
+<div class="row justify-content-center mt-3">
+    <div class="col-6">
+       <h2>Domain analyzer</h2>
+        <p>
+        This web page analyzer breaks down the structure and content of your web page and 
+        assesses the build quality and content quality from an accessibility, usability and 
+        search engine point of view.
+        </p>
+    </div>
+</div>
 @endsection
